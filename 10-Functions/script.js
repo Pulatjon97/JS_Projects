8097cd3fdeb4a91a9d0aa844ec561f2cb2ed02d6
Pulatjon <polatsultonov97@gmail.com>
2024-12-07 03:10:01 +0900

    Default Parameters

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -1,4 +1,29 @@
 'use strict';
-//dcdcdc
-// cdcsdsd
-// What the fuck is that?
+
+const bookings = [];
+
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  //this is ES6 solution
+  // ES5 solution
+  // numPassengers = numPassengers || 1;
+  // price = price || 199;
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH123', 2);
+createBooking('LH123', 5);
+
+createBooking('LH123', undefined, 1000);
