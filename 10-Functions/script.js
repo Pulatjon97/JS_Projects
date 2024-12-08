3e6f07db1f01b3ac9de8cabf74168fb6151ead1c
Pulatjon <polatsultonov97@gmail.com>
2024-12-08 20:11:20 +0900

    New Comment

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 const bookings = [];
 
 const createBooking = function (
@@ -27,3 +27,27 @@ createBooking('LH123', 2);
 createBooking('LH123', 5);
 
 createBooking('LH123', undefined, 1000);
+*/
+///////////////////////////////////////////
+// How Passing Arguments Works: Value vs. Reference
+
+const flight = 'LH234';
+const ali = {
+  name: 'Ali Sultan',
+  passport: 2343234234,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 2343234234) {
+    alert('Checked in');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+checkIn(flight, ali);
+console.log(flight);
+console.log(ali);
