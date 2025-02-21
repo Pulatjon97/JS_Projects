61f63aaf968f307921cbe3d325f3dd2b0a7ce75b
Pulatjon <polatsultonov97@gmail.com>
2025-02-21 17:40:30 +0900

    Remainder Operator  RP

--- Numbers,Dates,Intl and Timers/script.js
+++ Numbers,Dates,Intl and Timers/script.js
@@ -295,7 +295,7 @@ console.log(Number.isInteger(23 / 0));
 // Math and rounding
 /////////
 /////////
-
+/*
 console.log(Math.sqrt(25));
 console.log(25 ** (1 / 2));
 console.log(8 ** (1 / 3));
@@ -337,3 +337,29 @@ console.log((2.7).toFixed(0));
 console.log((2.234).toFixed(2));
 //console.log(Number(Number(2.234).toFixed(2))); // Research: ChatGPT's recommendation!
 console.log(+(2.234).toFixed(2)); // toFixed method gives a string value
+*/
+
+// Remainder Operator
+
+console.log(5 % 2);
+// console.log(Math.floor(5 / 2)); // Research!
+console.log(5 / 2);
+console.log(8 % 3);
+console.log(8 / 3); // 8 = 2 * 3 + 2
+
+console.log(7 % 2);
+console.log(7 / 2);
+
+const isEven = n => n % 2 === 0;
+console.log(isEven(8));
+console.log(isEven(823));
+console.log(isEven(514));
+
+labelBalance.addEventListener('click', function () {
+  [...document.querySelectorAll('.movements__row')].forEach(function (row, i) {
+    // 0, 2, 6, 8 ...
+    if (i % 2 === 0) row.style.backgroundColor = 'orangered';
+    // 0, 3, 6, 9 ...
+    if (i % 3 === 0) row.style.backgroundColor = 'blue';
+  });
+});
