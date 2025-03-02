e3e6d65ca767391378dacd80778f14b5075ec686
Pulatjon <polatsultonov97@gmail.com>
2025-03-02 09:44:40 +0900

    Styles, Attributes and Classes

--- Advanced DOM and Events/script.js
+++ Advanced DOM and Events/script.js
@@ -8,7 +8,8 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,8 +19,7 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -30,4 +30,93 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-/// New PRactice test
+// Selecting Elements
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+const header = document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
+// console.log(allSections);
+
+document.getElementById('section--1');
+const allButtons = document.getElementsByTagName('button');
+// console.log(allButtons);
+// console.log(allButtons);
+// console.log(document.getElementsByClassName('btn'));
+
+// Creating and Inserting Elements
+
+const message = document.createElement('div');
+message.insertAdjacentHTML(
+  'afterbegin',
+  "<p>Hi, I'm a message, Wossap nigga?</p>"
+);
+console.log(message);
+message.classList.add('cookie-message');
+// message.textContent =
+//   'We use cookied for improved functionalities and analytics';
+message.innerHTML =
+  'We use cookied for improved functionalities and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+
+// header.prepend(message);
+header.append(message);
+// header.append(message.cloneNode(true));
+
+// header.before(message);
+// header.after(message);
+
+// Delete Elements
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
+    message.remove(); // This one is new way of doing the same thing compared to the older version down below
+
+    // message.parentElement.removeChild(message); This is the old style removing the pop up messages. We may see this old way in some databases or websites because the developers who made this site may not know about it yet!
+  });
+
+// Styles
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+
+console.log(message.style.color);
+console.log(message.style.backgroundColor);
+
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height);
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+
+document.documentElement.style.setProperty('--color-primary', 'cyan');
+
+// Attributes
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.src);
+console.log(logo.className);
+
+logo.alt = 'Beautiful minimalist logo';
+
+// This one below is a non-standart way of doing it, that's why it is not working and gave undefined value
+console.log(logo.designer);
+console.log(logo.getAttribute('designer'));
+
+logo.setAttribute('company', 'Bankist');
+logo.getAttribute('src');
+console.log(logo.getAttribute('src'));
+
+const link = document.querySelector('.nav__link--btn');
+console.log(link.href);
+console.log(link.getAttribute('href'));
+
+// Data Attributes
+console.log(logo.dataset.versionNumber);
+
+// Classes
+logo.classList.add('c');
+logo.classList.remove('c');
+logo.classList.toggle('c');
+logo.classList.contains('c');
+
+//Don't use it because it will overwrite all the classes
+logo.className = 'jonas';
